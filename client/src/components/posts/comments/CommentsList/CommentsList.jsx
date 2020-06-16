6bbf4a988cf07a9a48e94bd49248f553147f8148
sameroso <samerkkr@gmail.com>
2020-06-16 08:30:26 -0300

    comment list style

--- client/src/components/posts/comments/CommentsList/CommentsList.jsx
+++ client/src/components/posts/comments/CommentsList/CommentsList.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Comment from '../Comment/Comment';
 
+import './CommentsList.scss';
+
 function CommentsList({ post }) {
   const [showComments, setShowComments] = useState(false);
   const commentsList = post.comments.map((comment) => (
@@ -17,17 +19,17 @@ function CommentsList({ post }) {
 
   const toggle = showComments ? (
     <>
-      <button onClick={() => setShowComments(false)}>
-        <span className="font">mostrar comentarios</span>
+      <button className="ui-95" onClick={() => setShowComments(false)}>
+        <span className="font">esconder comentarios</span>
       </button>
       <div>{commentsList}</div>
     </>
   ) : (
-    <button onClick={() => setShowComments(true)}>
-      <span className="font">esconder comentarios</span>
+    <button className="ui-95" onClick={() => setShowComments(true)}>
+      <span className="font">mostrar comentarios</span>
     </button>
   );
-  return <div className="mb-2 bg-primary">{toggle}</div>;
+  return <div className="mb-2 bg-comment-list-bottom-top pb-2">{toggle}</div>;
 }
 
 export default CommentsList;
