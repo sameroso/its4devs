6a90ca61f144c34b04fabb1eaec1e06ebddef7ee
sameroso <samerkkr@gmail.com>
2020-06-24 13:15:25 -0300

    notifications and changes on post btn

--- client/src/components/posts/PostForm/PostForm.jsx
+++ client/src/components/posts/PostForm/PostForm.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Field, reduxForm, submit } from 'redux-form';
 import { connect } from 'react-redux';
+import { toast } from 'react-toastify';
+import Loader from 'react-loader-spinner';
+
 import { sendPost } from '../../../actions';
 import InitialFormField from '../../InitialFormField/InitialFormField';
 
@@ -8,21 +11,49 @@ import './PostForm.scss';
 
 function PostForm({
   handleSubmit,
-  postData: { profileName, profilePic, sequenceId },
+  postData: { profileName, profilePic },
   sendPost,
   reset,
 }) {
-  const sequence = sequenceId ? sequenceId + 1 : 1;
-
   const postData = {
     profileName,
     profilePic,
     databasePostId: '1',
-    sequenceId: sequence,
   };
+  const [posting, setPosting] = useState(false);
+  const isPosting = posting ? (
+    <Loader type="ThreeDots" color="#000000" height={12} width={40} />
+  ) : (
+    'postar'
+  );
+
   const submitForm = async (formValues) => {
-    await sendPost({ ...postData, ...formValues });
-    reset();
+    try {
+      setPosting(true);
+      await sendPost({ ...postData, ...formValues });
+      toast.success('Postado com sucesso', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setPosting(false);
+      reset();
+    } catch {
+      toast.error('Erro! Tente mais tarde', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setPosting(false);
+    }
   };
   return (
     <div>
@@ -35,7 +66,7 @@ function PostForm({
         />
         <div className="row justify-content-end">
           <button type="submit" className="mr-5 font postform-btn">
-            <span className="postform-btn-text">Postar</span>
+            <span className="postform-btn-text">{isPosting}</span>
           </button>
         </div>
       </form>
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 
 import App from './components/App/App';
 import store from './configureStore';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure();
 
 ReactDOM.render(
   <Provider store={store}>
