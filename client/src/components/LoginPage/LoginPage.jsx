38355200e611faff9d5e4dd6595c446dfcfba5c8
sameroso <samerkkr@gmail.com>
2020-07-06 17:21:20 -0300

    Login Page with style changed

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -5,30 +5,49 @@ const LoginPage = () => {
   return (
     <>
       <div className="background">
-        <div className="container">
+        <div className="container LoginPage-container-position">
           <div className="row">
-            <span className="font mx-auto login-title">its4Devs</span>
+            <div className="mx-auto">
+              <span className="login-title">its</span>
+              <span className="login-title LoginPage-Number-Style">4</span>
+              <span className="login-title">Devs</span>
+            </div>
           </div>
-          <div className="row">
-            <a className="mx-auto mb-3" href="/auth/github">
-              <button className="windows-btn" data-test="github-btn">
-                <span className="font">login com github</span>
-              </button>
-            </a>
-          </div>
-          <div className="row mb-3">
-            <a className="mx-auto font" href="/auth/facebook">
-              <button className="windows-btn" data-test="facebook-btn">
-                login com facebook
-              </button>
-            </a>
-          </div>
-          <div className="row">
-            <a className="mx-auto font" href="/auth/google">
-              <button className="windows-btn" data-test="google-btn">
-                login com google
-              </button>
-            </a>
+          <div className="LoginPage-Card mx-auto">
+            <div className="LoginPage-bg-card-header mt-5">
+              <span className="LoginPage-font-card-header">
+                como deseja fazer login?
+              </span>
+            </div>
+            <div className="LoginPage-login-card-body">
+              <div className="row mt-3">
+                <a className="mx-auto LoginPage-Link-Size" href="/auth/github">
+                  <button className="windows-btn" data-test="github-btn">
+                    <span className="font">login com github</span>
+                  </button>
+                </a>
+              </div>
+              <div className="row mt-3 mb-3">
+                <a
+                  className="mx-auto font LoginPage-Link-Size"
+                  href="/auth/facebook"
+                >
+                  <button className="windows-btn" data-test="facebook-btn">
+                    login com facebook
+                  </button>
+                </a>
+              </div>
+              <div className="row mb-3">
+                <a
+                  className="mx-auto font LoginPage-Link-Size"
+                  href="/auth/google"
+                >
+                  <button className="windows-btn" data-test="google-btn">
+                    login com google
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
